2cb0843867d0e5e76f2a708b381f27578c2f84f1
Nayana Loivos <loivos.dev@gmail.com>
2021-12-06 19:14:31 -0300

    Aprendizado sobre objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,35 @@
+//Exercícios de interpretação de código:
+
+/* //1. a)
+Matheus Nachtergaele
+Virginia Cavendish
+{canal: "Globo", horario: "14h"}
+ */
+
+/* //2. a)
+{nome: 'Juca', idade: 3, raca: 'SRD'}
+{nome: 'Juba', idade: 3, raca: 'SRD'}
+{nome: 'Jubo', idade: 3, raca: 'SRD'} 
+
+//b) Um o objeto criado como modelo para aa const criadas.
+*/
+
+/* //3.a)
+false
+undefined
+
+b)Trouxe o valor false definido em "Backender" no objeto pessoa e undefined em "altura pois não 
+foi definido."
+ */
+
+//Exercícios de escrita de código:
+//1.a)
+const pessoa = {
+     nome: "Tatiane",
+     apelido: ['Tata', 'Tati', 'Ane']
+
+}
+function recebePessoa(pessoa){
+    console.log(`Eu sou ${nome} mas pode me chamar de ${apelido[0], [1], [2]}.`)
+
+}
